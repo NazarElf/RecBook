778cf9fdbb5b7347332980467ce35867e46cddf4
Nazar Y <nnn43@ukr.net>
2023-08-22 17:23:32 +0300

    clear button managed

--- client/src/components/CreateRecipeForm/CreateRecipeForm.jsx
+++ client/src/components/CreateRecipeForm/CreateRecipeForm.jsx
@@ -3,25 +3,25 @@ import FileBase from 'react-file-base64';
 import { useDispatch } from 'react-redux';
 import { createRecipe } from '../../actions/recipes';
 
-function getRandomInt(max) {
-    return Math.floor(Math.random() * max)+1;
-}
+const defaultState = { name: '', order: '', picture: '', ingridients: '' }
 
 const CreateRecipeForm = () => {
-    const [recipeData, setRecipeData] = useState({
-        name: '', order: '', picture: '', ingridients: ''
-    })
+    const [recipeData, setRecipeData] = useState(defaultState)
     const dispatch = useDispatch()
 
-    const handleClear = () => {
-
+    const handleClear = (e) => {
+        e.preventDefault();
+        setRecipeData(defaultState)
     }
     const handleSubmit = (e) => {
         e.preventDefault();
-        const recipeToSend = {...recipeData}
-        recipeToSend.ingridients = [{id:1, quantity:0.5, muType:"kilo"}]
-        
-        console.log("sending recipe: ",recipeToSend)
+
+        if (recipeData.name == 0 || recipeData.order == 0 || recipeData.picture == 0)
+            return;
+
+        const recipeToSend = { ...recipeData }
+        recipeToSend.ingridients = [{ id: 1, quantity: 0.5, muType: "kilo" }]
+
 
         dispatch(createRecipe(recipeToSend))
     }
@@ -32,26 +32,28 @@ const CreateRecipeForm = () => {
             <form>
                 <h4>Recipe Name:</h4>
                 <input
+                    required
                     type="text"
                     id="name" value={recipeData.name}
                     onChange={(e) => setRecipeData({ ...recipeData, name: e.target.value })}></input>
                 <h4>Creation Order:</h4>
                 <textarea
+                    required
                     name="order"
                     value={recipeData.order}
                     onChange={(e) => setRecipeData({ ...recipeData, order: e.target.value })}></textarea>
                 <h4>Upload Image:</h4>
                 <div>
                     <FileBase
-                    type="file"
-                    multiple={false}
-                    onDone={({base64})=> setRecipeData({...recipeData, picture: base64})}
+                        type="file"
+                        multiple={false}
+                        onDone={({ base64 }) => setRecipeData({ ...recipeData, picture: base64 })}
                     />
                 </div>
                 <h4>Ingridients:</h4>
                 <div>Not implemented yet :(</div>
 
-                <input type="submit" value="Create Recipe" onClick={handleSubmit}/>
+                <input type="submit" value="Create Recipe" onClick={handleSubmit} />
                 <button onClick={handleClear}>Clear</button>
             </form>
         </div>
